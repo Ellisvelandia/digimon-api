6ef4b890d2a2913108da880fa22694776034ef46
ellisvelandia <eyis619@gmail.com>
2022-09-05 20:08:16 -0500

    digimon

---
+++ .history/src/App_20220905200706.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      nst listaDigimon = [
+    {
+      name: "Agumon",
+      img: "https://digimon.shadowsmith.com/img/a...",
+      level: "Rookie"
+    },
+    {
+      name: "Gabumon",
+      img: "https://digimon.shadowsmith.com/img/g...",
+      level: "Rookie"
+    },
+    {
+      name: "Biyomon",
+      img: "https://digimon.shadowsmith.com/img/b...",
+      level: "Rookie"
+    },
+    {
+      name: "Tentomon",
+      img: "https://digimon.shadowsmith.com/img/t...",
+      level: "Rookie"
+    },
+    {
+      name: "Palmon",
+      img: "https://digimon.shadowsmith.com/img/p...",
+      level: "Rookie"
+    },
+    {
+      name: "Gomamon",
+      img: "https://digimon.shadowsmith.com/img/g...",
+      level: "Rookie"
+    },
+    {
+      name: "Patamon",
+      img: "https://digimon.shadowsmith.com/img/p...",
+      level: "Rookie"
+    }
+]
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+function App() {
+
+  const listaDigimon = [
+    {
+      name: "Agumon",
+      img: "https://digimon.shadowsmith.com/img/a...",
+      level: "Rookie"
+    },
+    {
+      name: "Gabumon",
+      img: "https://digimon.shadowsmith.com/img/g...",
+      level: "Rookie"
+    },
+    {
+      name: "Biyomon",
+      img: "https://digimon.shadowsmith.com/img/b...",
+      level: "Rookie"
+    },
+    {
+      name: "Tentomon",
+      img: "https://digimon.shadowsmith.com/img/t...",
+      level: "Rookie"
+    },
+    {
+      name: "Palmon",
+      img: "https://digimon.shadowsmith.com/img/p...",
+      level: "Rookie"
+    },
+    {
+      name: "Gomamon",
+      img: "https://digimon.shadowsmith.com/img/g...",
+      level: "Rookie"
+    },
+    {
+      name: "Patamon",
+      img: "https://digimon.shadowsmith.com/img/p...",
+      level: "Rookie"
+    }
+]
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
